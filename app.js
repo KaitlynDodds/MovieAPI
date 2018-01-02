fcf511506a53ec94811a4e8d1251282ac4969eb9
Kaitlyn Dodds <kaitlyndodds@Admins-MacBook-Pro.local>
2018-01-02 15:44:57 -0800

    Simple movie request setup

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+app.get('/', function(req, res) {
+    res.send("You made it to the home page.");
+});
+
+app.get('/results', function(req, res) {
+    request("http://www.omdbapi.com/?apikey=66d897ed&s=Harry", function(error, response, body) {
+        if (!error) {
+            var results = JSON.parse(body);
+            res.send(results);
+        }
+    });
+});
+
 app.set('port', process.env.PORT || 3000);
 
 app.listen(app.get('port'), function() {
